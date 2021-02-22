ff2421ad6fb58304e07a7c62009ba5fb619c8976
hreypar <hreypar@gmail.com>
2021-02-22 00:49:26 -0600

    useful variables

---
+++ config.mk
@@ -0,0 +1,21 @@
+# Variables 
+#
+# reference genome
+#
+REF_GENOME="references/GRCh38/Sequence/WholeGenomeFasta/genome.fa"
+
+# chromosomes to use in this analysis
+#
+MY_CHROMOSOMES="chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX"
+
+# Restriction enzyme
+#
+ENZYME="HindIII"
+
+# Digested genome for the Hi-C object
+#
+HIC_GENOME="digested_genome/GRCh38_HindIII.fa"
+
+# number of threads to use
+#
+N_THREADS="7"
